7368c6e0ee3131cfd56db0be131399cfbb39d4ba
Alex Berneira <alex@crfrs.org.br>
2025-06-19 19:52:43 -0300

    Revert: Remover todas as tentativas de deploy e voltar ao estado original

--- test-ftp.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Arquivo de teste para verificar se o FTP está funcionando
-echo "✅ FTP Test - Se você está vendo esta mensagem, o upload funcionou!";
-echo "<br>";
-echo "Data/Hora: " . date('d/m/Y H:i:s');
-echo "<br>";
-echo "Servidor: " . $_SERVER['SERVER_NAME'];
-?> 
