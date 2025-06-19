7cd50b89c2fdb7d1e155175656c24998a491019e
Alex Berneira <alex@crfrs.org.br>
2025-06-19 19:15:03 -0300

    Add: Arquivo de teste FTP e workflow para testar apenas este arquivo

---
+++ test-ftp.php
@@ -0,0 +1,8 @@
+<?php
+// Arquivo de teste para verificar se o FTP está funcionando
+echo "✅ FTP Test - Se você está vendo esta mensagem, o upload funcionou!";
+echo "<br>";
+echo "Data/Hora: " . date('d/m/Y H:i:s');
+echo "<br>";
+echo "Servidor: " . $_SERVER['SERVER_NAME'];
+?> 
